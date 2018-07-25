946dbea30aad3ba22d8d5046c6caee2a75867091
Fiona Wilson <fionaw237@gmail.com>
2018-07-25 21:44:04 +0100

    Adds guess_letter, is_lost? and is_won? functions, with appropriate tests

--- snowman/game.rb
+++ snowman/game.rb
@@ -12,4 +12,28 @@ class Game
     return @guessed_letters.count()
   end
 
+  def guess_letter(word, letter)
+    @guessed_letters << letter
+    if word.letter_in_word(letter) == false
+      @player.lose_a_life()
+    end
+  end
+
+  def is_lost?()
+    if @player.lives() == 0
+      return true
+    else
+      return false
+    end
+  end
+
+  def is_won?()
+    if @word.display_guessed_letters(@guessed_letters) == @word.word()
+      return true
+    else
+      return false
+    end
+  end
+
+
 end
--- snowman/specs/game_spec.rb
+++ snowman/specs/game_spec.rb
@@ -24,4 +24,52 @@ class GameTest < MiniTest::Test
     assert_equal(0, @game.guessed_letters_count())
   end
 
+  def test_guess_letter__not_in_word()
+    @game.guess_letter(@word, "m")
+    assert_equal(1, @game.guessed_letters_count())
+    assert_equal(5, @player.lives())
+  end
+
+  def test_guess_letter__in_word()
+    @game.guess_letter(@word, "s")
+    assert_equal(1, @game.guessed_letters_count())
+    assert_equal(6, @player.lives())
+  end
+
+  def test_game_lost__true()
+    letters_to_guess = ["a", "b", "c", "e", "f", "g"]
+    letters_to_guess.each do |letter|
+      @game.guess_letter(@word, letter)
+    end
+    result = @game.is_lost?()
+    assert_equal(true, result)
+  end
+
+  def test_game_lost__false()
+    letters_to_guess = ["a", "b", "c", "d", "f", "g"]
+    letters_to_guess.each do |letter|
+      @game.guess_letter(@word, letter)
+    end
+    result = @game.is_lost?()
+    assert_equal(false, result)
+  end
+
+  def test_game_won__true()
+    letters_to_guess = ["d", "u", "s", "k", "y"]
+    letters_to_guess.each do |letter|
+      @game.guess_letter(@word, letter)
+    end
+    result = @game.is_won?()
+    assert_equal(true, result)
+  end
+
+  def test_game_won__false()
+    letters_to_guess = ["d", "u", "s", "k", "z"]
+    letters_to_guess.each do |letter|
+      @game.guess_letter(@word, letter)
+    end
+    result = @game.is_won?()
+    assert_equal(false, result)
+  end
+
 end
