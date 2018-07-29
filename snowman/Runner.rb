f8f67a8e705d8e993b7f4bd6e48163d7302bd333
Fiona Wilson <fionaw237@gmail.com>
2018-07-29 12:16:01 +0100

    Removes old runner file

--- snowman/Runner.rb
+++
@@ -1,7 +0,0 @@
-require_relative("game.rb")
-require_relative("player.rb")
-require_relative("hidden_word.rb")
-require 'io/console'
-
-puts "Enter a secret word: "
-word = STDIN.noecho(&:gets).chomp
