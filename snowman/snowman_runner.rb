4107fce1aab84ec8cba9ca513fe0b384ec978ded
Fiona Wilson <fionaw237@gmail.com>
2018-07-29 12:27:44 +0100

    Adds minor modifications

--- snowman/snowman_runner.rb
+++ snowman/snowman_runner.rb
@@ -3,17 +3,20 @@ require_relative("player.rb")
 require_relative("hidden_word.rb")
 require 'io/console'
 
-
-puts "Enter a secret word: "
+puts "Enter a secret word/phrase: "
 # entered_word = STDIN.noecho(&:gets).chomp
 entered_word = gets.chomp()
 system 'clear'
 
+puts "What's your name?"
+name = gets.chomp()
+puts "Hi, #{name}!"
+
+player = Player.new(name)
 word = HiddenWord.new(entered_word)
-player = Player.new("Fiona")
 game = Game.new(player, word)
 
-puts "The word to guess is ", word.display_guessed_letters(word.display_guessed_letters(game.guessed_letters()))
+puts "Your word/phrase to guess is ", word.display_guessed_letters(word.display_guessed_letters(game.guessed_letters()))
 
 while (!game.is_won) && (!game.is_lost)
   puts "Guess a letter: "
@@ -24,5 +27,5 @@ while (!game.is_won) && (!game.is_lost)
   game.is_lost?()
 end
 
-puts "Well done, you win!" if game.is_won()
+puts "Well done, #{name} - you win!" if game.is_won()
 puts "You lose - snowman has melted :(" if game.is_lost()
